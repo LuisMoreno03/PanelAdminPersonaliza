acfca8639b5de54ad29d99e46401fa034f5fc39e
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2025-12-12 16:08:10 -0400

    1.2.2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,19 +6,16 @@ use App\Controllers\DashboardController;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Auth::index');
 $routes->post('auth/login', 'Auth::login');
-$routes->get('dashboard', 'Dashboard::index');
 
-/** rutas de conexion con shopify */
-$routes->get('api/getOrders', 'ShopifyController::getOrders');
+/* DASHBOARD */
+$routes->get('dashboard', 'DashboardController::index');
 $routes->get('dashboard/filter', 'DashboardController::filter');
 $routes->get('dashboard/filter/(:any)', 'DashboardController::filter/$1');
-
-
-$routes->post('api/estado/guardar', 'DashboardController::guardarEstado');
-$routes->post('api/estado/etiquetas/guardar', 'DashboardController::guardarEtiquetas');
 $routes->get('dashboard/sync', 'DashboardController::syncPedidos');
 
-
-
+/* API */
+$routes->post('api/estado/guardar', 'DashboardController::guardarEstado');
+$routes->post('api/estado/etiquetas/guardar', 'DashboardController::guardarEtiquetas');
