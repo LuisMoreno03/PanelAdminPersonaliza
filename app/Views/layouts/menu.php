dfd2eeed659773e5d72dddc61f8c66aa7e3611e0
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2025-12-14 14:13:28 -0400

    1.2.6.7

--- app/Views/layouts/menu.php
+++ app/Views/layouts/menu.php
@@ -20,7 +20,7 @@
             Panel Admin
         </div>
         <div>
-            <p>Usuario: <?php echo $_SESSION['user']; ?></p>
+            <p>Usuario: <?php echo $_SESSION['nombre']; ?></p>
         </div>
 
         <nav class="mt-6 space-y-2 px-4">
