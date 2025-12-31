c2da78f5c34a3fc5a5edeec80c5cb61aa7de5b87
AdrianRhg <adrianhernandez18.12.04@gmail.com>
2025-12-31 15:09:35 -0400

    1.3.1.3

--- index.php
+++ index.php
@@ -62,16 +62,4 @@ exit(CodeIgniter\Boot::bootWeb($paths));
 
 
 
-// APARTADO DE PEDIDOS 
-<!doctype html>
-<html lang="es">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Pedidos</title>
-</head>
-<body style="font-family: Arial; padding: 20px;">
-  <h1>✅ Ruta /pedidos funcionando</h1>
-  <p>La vista <b>app/Views/pedidos/index.php</b> está cargando correctamente.</p>
-</body>
-</html>
+
