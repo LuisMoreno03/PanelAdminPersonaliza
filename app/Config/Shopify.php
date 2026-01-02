4dbe0f0efee593fa9a370f1249bdcf26d34f2ada
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2026-01-02 10:51:49 -0400

    1.3.1.99

--- app/Config/Shopify.php
+++ app/Config/Shopify.php
@@ -8,5 +8,5 @@ class Shopify extends BaseConfig
 {
     public string $shop='962f2d.myshopify.com';
     public string $token='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';
-    public string $apiVersion = '2024-01';
+    public string $apiVersion = '2025-10';
 }
