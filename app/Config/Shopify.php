75aa57f94df7264d90001ce3335fabf4f5ac7123
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2026-01-02 09:26:49 -0400

    1.3.1.97

--- app/Config/Shopify.php
+++ app/Config/Shopify.php
@@ -6,7 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 
 class Shopify extends BaseConfig
 {
-    public string $shop  = '962f2d.myshopify.com';
-    public string $token = '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';
+    public string $shop='962f2d.myshopify.com';
+    public string $token='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';
     public string $apiVersion = '2024-01';
 }
