8d8159ed39b4c53a40c782ca64435b5b4f7f4fc9
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2026-01-11 23:01:45 -0400

    1.3.3.20

--- app/Views/layouts/menu.php
+++ app/Views/layouts/menu.php
@@ -255,7 +255,7 @@
                       d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 9m13-9l2 9M10 21a1 1 0 100-2 1 1 0 000 2zm8 0a1 1 0 100-2 1 1 0 000 2z"/>
               </svg>
             </span>
-            <span class="text-sm font-semibold text-white/90">Producción</span>
+            <span class="text-sm font-semibold text-white/90">Diseño</span>
           </a>
 
           <a href="<?= base_url('confirmados') ?>"
