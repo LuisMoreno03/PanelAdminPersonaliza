969f756989101b0aa98f556ddcce65b2236cd784
AdrianRhg <adrianhernandez18.12.04@gmail.com>
2026-01-20 18:42:35 -0400

    1.3.3.28

--- app/Views/usuarios.php
+++ app/Views/usuarios.php
@@ -5,13 +5,16 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\UsuariosModel;
 
-
+class Usuarios extends BaseController
 {
-
-
+public function index()
 {
-  return view('Usuarios');
+    public function index()
+{
+    return view('usuarios_index');
+
 }
 
 }
 
+}
