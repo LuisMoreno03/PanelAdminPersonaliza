17a78f0fe505092826aa07c01a5a373cacc0fca0
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2026-01-20 11:24:14 -0400

    1.3.3.28

--- app/Controllers/SupportController.php
+++ app/Controllers/SupportController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use CodeIgniter\HTTP\ResponseInterface;
 
-class SoporteController extends BaseController
+class SupportController extends BaseController
 {
   protected $db;
 
