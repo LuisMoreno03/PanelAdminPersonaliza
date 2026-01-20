d2e9c2e12ba7f20f8bddf5f84d38ac149bf35c63
AdrianRhg <adrianhernandez18.12.04@gmail.com>
2026-01-20 19:33:23 -0400

    1.3.3.28

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -212,7 +212,7 @@ $routes->group('repetir', [
 |--------------------------------------------------------------------------
 */
 $routes->group('', ['namespace' => 'App\Controllers'], function($routes) {
-    $routes->get('usuarios', 'UsuariosController::index');
+    $routes->get('usuarios', 'usuarios::index');
     $routes->get('usuarios/(:num)/password', 'UsuariosController::password/$1');
     $routes->post('usuarios/(:num)/password', 'UsuariosController::updatePassword/$1');
 });
--- app/Views/usuarios.php
+++ app/Views/usuarios.php
@@ -5,21 +5,12 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\UsuarioModel;
 
-class Usuarios extends BaseController
+class usuarios extends BaseController
 {
-    protected UsuarioModel $usuarioModel;
-
-    public function __construct()
-    {
-        $this->usuarioModel = new UsuarioModel();
-    }
-
+    
     public function index()
     {
-        return 'Usuarios::index OK';
-        // luego volvemos a la vista
+        return 'UsuariosController::index OK';
     }
 
-    public function password(int $id) {}
-    public function updatePassword(int $id) {}
 }
