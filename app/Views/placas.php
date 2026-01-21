adf05264dfa13b1bd1f939fd3b09afba848780bc
AdrianRhg <adrianhernandez18.12.04@gmail.com>
2026-01-21 10:06:47 -0400

    1.3.3.30

--- app/Views/placas.php
+++ app/Views/placas.php
@@ -264,7 +264,7 @@
     <!-- Body -->
     <div class="p-6 space-y-3">
       <!-- ✅ Producto -->
-      <input id="cargaProducto" type="text" placeholder="Producto"
+      <input id="cargaProducto" type="text" placeholder="Producto (opcional)"
              class="w-full border rounded-xl px-3 py-2">
 
       <!-- ✅ Nombre del lote (visible) -->
