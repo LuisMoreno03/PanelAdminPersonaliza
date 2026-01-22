2e12a84f7cc18fb0a781f3d67d8a688d0573275e
AdrianRhg <adrianhernandez18.12.04@gmail.com>
2026-01-22 15:56:52 -0400

    1.3.3.31

--- app/Views/chat.php
+++ app/Views/chat.php
@@ -207,6 +207,8 @@
 
   // ===== Config =====
  // ===== Config =====
+   // ===== Config =====
+const SOCKET_URL = "https://paneladministrativopersonaliza.com:3001";
 const ADMIN_ID = <?= (int)session('user_id') ?>;
 const ADMIN_NAME = <?= json_encode(session('nombre') ?? 'Admin') ?>;
 
