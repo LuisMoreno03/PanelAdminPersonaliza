398505c2e0d1110d8094b384cb534be80ac42378
Luis Moreno <79389515+LuisMoreno03@users.noreply.github.com>
2026-01-29 06:09:09 -0400

    1.3.3.42

--- app/Controllers/PorProducirController.php
+++ app/Controllers/PorProducirController.php
@@ -13,7 +13,7 @@ class PorProducirController extends Controller
      */
     public function index()
     {
-        return view('porProducir');
+        return view('porproducir');
     }
 
     /**
